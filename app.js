db429719cb8749b5f632baeca2c4eb1286a00fd1
Chathuranga Wijesinghe <crwijesinghe@gmail.com>
2020-01-22 18:47:28 +0800

    styling our pages

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const path = require('path');
+const path = require("path");
 
 const express = require("express");
 const bodyParser = require("body-parser");
@@ -10,11 +10,11 @@ app.use(bodyParser.urlencoded({ extended: false }));
 const adminRoutes = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
 
-app.use('/admin', adminRoutes);
+app.use("/admin", adminRoutes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-    res.status(404).sendFile(path.join(__dirname,'views', '404.html'));
+  res.status(404).sendFile(path.join(__dirname, "views", "404.html"));
 });
 
 app.listen(3000);
--- routes/admin.js
+++ routes/admin.js
@@ -1,14 +1,14 @@
-const path = require('path');
+const path = require("path");
 
 const express = require("express");
 
-const rootDir = require('../util/path');
+const rootDir = require("../util/path");
 
 const router = express.Router();
 
 // /admin/add-product => GET
 router.get("/add-product", (req, res, next) => {
-    res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
+  res.sendFile(path.join(rootDir, "views", "add-product.html"));
 });
 
 // /admin/add-product => POST
